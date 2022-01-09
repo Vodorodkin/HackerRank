4ae619f8e4b405d74c25aa98b576d3a91b59a373
d.vodorodkin <vodorodkin@gmail.com>
2022-01-09 23:57:07 +0300

    - Added an implemented method in LeftRotation.cs - Added TwoDArraysDs.cs with an implemented method

--- HackerRank/InterviewPreparationKit/Arrays/LeftRotation.cs
+++ HackerRank/InterviewPreparationKit/Arrays/LeftRotation.cs
@@ -1,7 +1,41 @@
-﻿namespace HackerRank.InterviewPreparationKit.Arrays
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace HackerRank.InterviewPreparationKit.Arrays
 {
     public class LeftRotation
     {
-        
+        /*
+         * Complete the 'rotLeft' function below.
+         *
+         * The function is expected to return an INTEGER_ARRAY.
+         * The function accepts following parameters:
+         *  1. INTEGER_ARRAY a
+         *  2. INTEGER d
+         */
+
+        public static List<int> rotLeft_v1(List<int> a, int d)
+        {
+            int[] leftRotatedArray = new int[a.Count];
+            int indexRotated = d % a.Count;
+            
+            for (int i = 0; i < a.Count; i++)
+            {
+                int newIndex;
+                
+                if (indexRotated > i)
+                {
+                    newIndex = a.Count - indexRotated + i;
+                }
+                else
+                {
+                    newIndex = i - indexRotated;
+                }
+                
+                leftRotatedArray[newIndex] = a[i];
+            }
+
+            return leftRotatedArray.ToList();
+        }
     }
 }
---
+++ HackerRank/InterviewPreparationKit/Arrays/TwoDArraysDs.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HackerRank.InterviewPreparationKit.Arrays
+{
+    public class TwoDArraysDs {
+        
+        /*
+         * Complete the 'hourglassSum' function below.
+         *
+         * The function is expected to return an INTEGER.
+         * The function accepts 2D_INTEGER_ARRAY arr as parameter.
+         */
+        
+        public static int hourglassSum_v1(List<List<int>> arr)
+        {
+            int? maxSum = null;
+            // "List<(int, int)>" don't supported on HackerRank
+            List<Tuple<int, int>> hourglassPointers = new List<Tuple<int, int>>
+            {
+                // "New ()" don't supported on HackerRank
+                new Tuple<int, int> (0, 0),
+                new Tuple<int, int> (0, 1),
+                new Tuple<int, int> (0, 2),
+                new Tuple<int, int> (1, 1),
+                new Tuple<int, int> (2, 0),
+                new Tuple<int, int> (2, 1),
+                new Tuple<int, int> (2, 2)
+            };
+
+            for (int i = 0; i < arr.Count - 2; i++)
+            {
+                List<int> internalArray = arr[i];
+
+                for (int j = 0; j < internalArray.Count - 2; j++)
+                {
+                    int sum = 0;
+                    
+                    foreach (var pointer in hourglassPointers)
+                    {
+                        sum += arr[i + pointer.Item1][j + pointer.Item2];
+                    }
+
+                    if (maxSum == null || sum > maxSum)
+                    {
+                        maxSum = sum;
+                    }
+                }
+            }
+
+            return maxSum ?? 0;
+        }
+    }
+}
